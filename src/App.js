b4821b621611a8b49a605581f5a70284d63ddb12
Nafiseh Ebrahimi <ebranafas@gmail.com>
2021-08-27 18:23:00 +0430

    create consept of timer

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { useState } from 'react'
 
 function App() {
 
-    const [state, setState] = useState(false);
-
-    console.log(state);
+    const [isSetTime, setIsSetTime] = useState(false);
+    const [stateTrue, setStateTrue] = useState(false);
 
     return (
         <React.Fragment>
-            <h1>hello world :)</h1>
-            <button onClick={() => setState(!state)}>{state.toString()}</button>
+            {
+                !isSetTime ?
+                    <div>
+                        <input type="text" />
+                        <button onClick={(e) => setIsSetTime(true)}>set time!</button>
+                    </div>
+                :
+                <div>
+                    <h1>hello world :)</h1>
+                    <button onClick={() => setStateTrue(!stateTrue)}>
+                        {stateTrue.toString()}
+                    </button>
+                </div>
+            }
         </React.Fragment>
     )
 }
